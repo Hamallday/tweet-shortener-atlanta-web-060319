f9d9c99c535f4272ed191f5fef6873620076b071
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-06-02 01:13:17 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -16,10 +16,10 @@ def dictionary
 }
 end
 
-def word_substituter(tweet)
+def word_substituter(tweets)
 
 
-    tweet.split(" ").map do |word|
+    tweets.split(" ").map do |word|
       if dictionary.keys.include?(word.downcase)
         word = dictionary[word.downcase]
       else
@@ -28,14 +28,14 @@ def word_substituter(tweet)
   end.join(" ")
 end
 
-def bulk_tweet_shortener(tweet)
-  tweet.map do |phrase|
+def bulk_tweet_shortener(tweets)
+  tweets.map do |phrase|
     puts word_substituter(phrase)
   end
 end
 
-def selective_tweet_shortener(tweet)
-  tweet.split(" ").map do |phrase|
+def selective_tweet_shortener(tweets)
+  tweets.split(" ").map do |phrase|
     if phrase.length > 140
       word_substituter(phrase)
     elsif phrase.length <= 140
@@ -44,8 +44,8 @@ def selective_tweet_shortener(tweet)
   end
 end
 
-def shortened_tweet_truncator(tweet)
-  tweet.split(" ").map do |phrase|
+def shortened_tweet_truncator(tweets)
+  tweets.split(" ").map do |phrase|
     if phrase.length > 140
       word_substituter(phrase)[0..140] + "..."
     else
